e46aed24e5da6349fce2d778550aa426c0d1a94e
nikhil nikhil <nikhilthakur050@gmail.com>
2019-07-17 18:42:51 -0400

    Initial Commit

---
+++ day14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day14
+//
+//  Created by User on 2019-07-17.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
